295e91bcc7e4fc52f7e757b2d54fbd3e6f76d9e3
SofiyaChe <chebotarevasophie@yandex.ru>
2023-04-19 08:02:47 +0300

    commit 19/04

--- src/main/java/com/game/entity/Player.java
+++ src/main/java/com/game/entity/Player.java
@@ -4,10 +4,11 @@ import java.util.Date;
 
 @Entity
 @Table(schema = "rpg", name = "player")
-
+@NamedQuery(name = "player_getAllCount", query = "select count(p) from Player p")
 
 public class Player {
     @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     @Column(nullable = false)
     private Long id;
 
--- src/main/java/com/game/repository/PlayerRepositoryDB.java
+++ src/main/java/com/game/repository/PlayerRepositoryDB.java
@@ -3,9 +3,11 @@ package com.game.repository;
 import com.game.entity.Player;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
 import org.hibernate.cfg.Configuration;
 import org.hibernate.cfg.Environment;
 import org.hibernate.query.NativeQuery;
+import org.hibernate.query.Query;
 import org.springframework.stereotype.Repository;
 
 //import javax.annotation.PreDestroy;
@@ -21,10 +23,10 @@ public class PlayerRepositoryDB implements IPlayerRepository {
     public PlayerRepositoryDB() {
         Properties properties = new Properties();
         properties.put(Environment.DIALECT, "org.hibernate.dialect.MySQL8Dialect");
-        properties.put(Environment.DRIVER, "com.p6spy.engine.spy.P6SpyDriver");
-        properties.put(Environment.URL, "jdbc:p6spy:mysql://localhost:3306/rpg");
+        properties.put(Environment.DRIVER, "com.p6spy.engine.spy.P6SpyDriver"); //com.mysql.cj.jdbc.Driver
+        properties.put(Environment.URL, "jdbc:p6spy:mysql://localhost:3306/rpg"); //jdbc:mysql://localhost:3306/rpg
         properties.put(Environment.USER, "root");
-        properties.put(Environment.PASS, "root");
+        properties.put(Environment.PASS, "$RFVcde3");
         properties.put(Environment.HBM2DDL_AUTO, "update");
 
         sessionFactory = new Configuration()
@@ -46,31 +48,52 @@ public class PlayerRepositoryDB implements IPlayerRepository {
 
     @Override
     public int getAllCount() {
-        return 0;
+        try(Session session = sessionFactory.openSession()) {
+           Query<Long> query =  session.createNamedQuery("player_getAllCount", Long.class);
+           return Math.toIntExact(query.uniqueResult());
+        }
     }
 
     @Override
     public Player save(Player player) {
-        return null;
+        try(Session session = sessionFactory.openSession()) {
+            Transaction transaction = session.beginTransaction();
+            session.save(player);
+            transaction.commit();
+            return player;
+        }
+
     }
 
     @Override
     public Player update(Player player) {
-        return null;
+        try(Session session = sessionFactory.openSession()) {
+            Transaction transaction = session.beginTransaction();
+            session.update(player);
+            transaction.commit();
+            return player;
+        }
     }
 
     @Override
     public Optional<Player> findById(long id) {
-        return Optional.empty();
+        try(Session session = sessionFactory.openSession()) {
+           Player player = session.find(Player.class, id);
+           return Optional.of(player);
+        }
     }
 
     @Override
     public void delete(Player player) {
-
+        try(Session session = sessionFactory.openSession()) {
+            Transaction transaction = session.beginTransaction();
+            session.remove(player);
+            transaction.commit();
+        }
     }
 
-    //@PreDestroy
+   // @PreDestroy
     public void beforeStop() {
-
+        sessionFactory.close();
     }
 }
